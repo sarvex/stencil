e8844d4a2618e98a47c09225fde7d311b154e898
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2023-04-06 14:18:36 -0400

    chore(release): override angular issue prefixes
    this commit introduces a configuration file for conventional changelog,
    in order to override the angular presets that stencil uses today. the
    motivation for this change is that when a stencil developer cross
    references an issue in stencil in a commit issue, conventional-changelog
    + the angular preset assumes it's an issue number that's been closed.
    when writing the changelog, text "closes #ISSUE_NUMBER" is erroneously
    added to CHANGELOG.md, which has to be manually fixed at release time.
    
    doing this helps in the immediate future (the engineer performing the
    release doesn't have to manually update CHANGELOG.md) and will be useful
    when the team enables ci-based releases.

---
+++ scripts/utils/conventional-changelog-config.ts
@@ -0,0 +1,19 @@
+/**
+ * Options for conventional changelog.
+ */
+const options = {
+  parserOpts: {
+    /**
+     * Override the conventional-changelog parser's preset for detecting issues. Stencil uses the "Angular preset",
+     * which defaults the "issuesPrefixes" field to a single pound sign ('#'). This sometimes gets mistaken by the
+     * changelog generator as an issue that is fixed, when it fact it's cross-reference to another issue.
+     *
+     * Reference for this property: [GitHub README]{@link https://github.com/conventional-changelog/conventional-changelog/tree/master/packages/conventional-commits-parser#issueprefixes}
+     *
+     * By default, [these are case-insensitive]{@link https://github.com/conventional-changelog/conventional-changelog/tree/master/packages/conventional-commits-parser#issueprefixescasesensitive)}
+     */
+    issuePrefixes: ['closes: #', 'closes:#', 'closes- #', 'closes-#', 'closes #', 'closes#'],
+  },
+};
+
+export default options;
