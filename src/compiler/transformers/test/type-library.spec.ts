7c9e6c7388c6e32d63d050d2205d99ecbd8ce7ea
Alice Pote <alice.writes.wrongs@gmail.com>
2023-04-07 10:01:14 -0400

    gahhh!!!! I'm going to write end-to-end tests instead!!!

--- src/compiler/transformers/test/type-library.spec.ts
+++ src/compiler/transformers/test/type-library.spec.ts
@@ -5,6 +5,7 @@ import { addToLibrary, getTypeLibrary } from '../type-library';
 import ts from 'typescript';
 
 import { patchTypescript } from '../../sys/typescript/typescript-sys';
+import { getAllTypeReferences } from '../transform-utils';
 
 function resetLibrary() {
   const library = getTypeLibrary();
@@ -17,38 +18,69 @@ function resetLibrary() {
 async function setup() {
   const compilerContext: CompilerCtx = mockCompilerCtx();
   // TODO need the config or no?
-  const config = mockValidatedConfig();
+  const config = mockValidatedConfig({
+    rootDir: path.resolve("."),
+  });
 
   patchTypescript(config, compilerContext.fs);
 
   // we need to have files in the `inMemoryFs` which TypeScript
   // can resolve, otherwise it won't find the module!
+  // const dessertModulePath = path.join(config.rootDir, 'src/dessert.ts');
+  const dessertModulePath = 'src/dessert.ts';
   await compilerContext.fs.writeFile(
-    path.join(config.rootDir, 'src/dessert.ts'),
+    dessertModulePath,
     `export interface Pie {
   type: 'pumpkin' | 'apple' | 'pecan';
   diameter: number;
 }`
   );
+
+  // const mealModulePath = path.join(config.rootDir, 'src/meal.ts');
+  const mealModulePath = 'src/meal.ts';
   await compilerContext.fs.writeFile(
-    path.join(config.rootDir, 'src/meal.ts'),
-    `export function prepareMeal (pie: Pie) {
+    mealModulePath,
+    `import { Pie } from './dessert';
+
+    export function prepareMeal(pie: Pie) {
       eat(pie);
     }
 
     function eat(pie: Pie) {
       console.log('I ate it');
-    }`
+    }`,
   );
 
-  const program = ts.createProgram(['src/dessert.ts'], {});
+  const options = {
+    ...ts.getDefaultCompilerOptions(),
+    module: ts.ModuleKind.ESNext,
+    moduleResolution: ts.ModuleResolutionKind.NodeJs,
+    rootDir: 'src/',
+    baseUrl: '.',
+  };
+
+  const program = ts.createProgram(['src/meal.ts'], {
+    ...options,
+  }, ts.createCompilerHost(options));
+
+  console.log('SOURCE FILES::', program.getSourceFiles().length)
+
   const checker = program.getTypeChecker();
+  const mealModule = program.getSourceFile(mealModulePath);
+  console.log(mealModule);
+  return { program, checker, mealModule };
 }
 
 describe('type library', () => {
-  beforeEach(() => {
+  afterEach(() => {
     resetLibrary();
   });
 
-  it('should be able to add a type to the library', () => {});
+  it('should be able to add a type to the library', async () => {
+    const { checker, mealModule } = await setup();
+    const referencedtypes = getAllTypeReferences(checker, mealModule);
+    referencedtypes.forEach(({ name, type }) => {
+      addToLibrary(type, name, checker);
+    });
+  });
 });
