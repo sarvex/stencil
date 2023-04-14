251436588f5796ec2cb06d14b3be27e421855dfb
Alice Pote <alice.writes.wrongs@gmail.com>
2023-04-14 09:45:53 -0400

    alrighty

--- test/docs-json/src/components/interfaces.ts
+++ test/docs-json/src/components/interfaces.ts
@@ -1,18 +1,36 @@
+/**
+ * Interface that should be included
+ */
 export interface Pie {
   type: 'pumpkin' | 'apple' | 'pecan';
   name: string;
   diameter: number;
 }
 
+/**
+  * Type that should be included
+ *
+ */
 export type FooBar = {
   biz: string;
 };
 
+/**
+ * Enum that should be included
+ */
 export enum FizzBuzz {
   One,
   Two,
   Three,
 }
 
-export type { NotUsedInterface as BestInterface } from './test-not-used';
+// re-export w/ alias and `export type`, should be under original name in `docs.json`
+export type { ReExportedUnderNewNameWithType as BestInterface } from './test-not-used';
+// re-export w/ alias, should be under original name in `docs.json`
+export { ReExportedUnderNewName as BetterInterface } from './test-not-used';
+// re-export w/ `export type`
+export type { ReExportedWithType } from './test-not-used';
+// re-export
+export { ReExported } from './test-not-used';
+
 export * from './test-not-used';
--- test/docs-json/src/components/test-not-used.ts
+++ test/docs-json/src/components/test-not-used.ts
@@ -1,7 +1,34 @@
-export interface NotUsedInterface {
+/**
+ * If I show up then a re-export w/ alias and `export type` works!
+ */
+export interface ReExportedUnderNewNameWithType {
   test: string;
 }
 
-export interface AnotherInterface {
+/**
+ * If I show up then a re-export w/ alias works!
+ */
+export interface ReExportedUnderNewName {
+  test: string;
+}
+
+/**
+ * If I show up then a re-export works!
+ */
+export interface ReExported {
+  test: string;
+}
+
+/**
+ * If I show up then a re-export w/ `export type` works!
+ */
+export interface ReExportedWithType {
+  test: string;
+}
+
+/**
+ * If I show up then a `export * from '...'` works!
+ */
+export interface IncludedInWildcard {
   test: string;
 }
